235cd0dd94dcce7e5c2324c780eb20f399f9cf11
Bartosz Różański <bartek.roza@gmail.com>
2022-12-04 21:50:26 +0100

    Complete day 1/2022

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -1,14 +1,21 @@
-fn main() {
-    let mut max = 0;
-    let mut current = 0;
-    for line in include_str!("input.txt").lines() {
-        if line.is_empty() {
-            max = std::cmp::max(max, current);
-            current = 0;
-        } else {
-            current += line.parse::<usize>().unwrap();
-        }
-    }
+use itertools::Itertools;
 
-    println!("{}",max)
+fn main() {
+    let elves: Vec<usize> = include_str!("input.txt")
+        .lines()
+        .group_by(|l| l.is_empty())
+        .into_iter()
+        .filter_map(|(empty, elf)| {
+            if !empty {
+                Some(elf.map(|e| e.parse::<usize>().unwrap()).sum())
+            } else {
+                None
+            }
+        })
+        .sorted()
+        .rev()
+        .take(3)
+        .collect();
+    println!("PART 1: {}", elves[0]);
+    println!("PART 2: {}", elves.iter().sum::<usize>());
 }
